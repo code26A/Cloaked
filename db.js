a9ec3fcf34c9f125ecdcccbbea1904deeac25579
Siddharth Shankar <104447344+code26A@users.noreply.github.com>
2025-09-20 15:06:29 +0530

    Delete db.js

--- db.js
+++
@@ -1,8 +0,0 @@
-import { Sequelize } from 'sequelize';
-
-const sequelize = new Sequelize('cloakdb', 'postgres', 'Madhav#2003', {
-    host: 'localhost',
-    dialect: 'postgres'
-});
-
-export { sequelize };
